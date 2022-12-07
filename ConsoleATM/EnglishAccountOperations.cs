ab11569a6626589fe927b88394d7bfce5ad9420e
Bello Soliu <95251843+techflow21@users.noreply.github.com>
2022-12-07 21:16:24 +0100

    Update EnglishAccountOperations.cs

--- ConsoleATM/EnglishAccountOperations.cs
+++ ConsoleATM/EnglishAccountOperations.cs
@@ -63,7 +63,7 @@
 
                 default:
                     {
-                        Console.WriteLine("You entered invalid Operation Option");
+                        Console.WriteLine("You entered invalid operation Option");
                     }
                     break;
             }
